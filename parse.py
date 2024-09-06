a634f9fa393ed95303ff501325851eb18ae11fc7
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-09-06 14:04:41 -0700

    Added a start line number argument

--- parse.py
+++ parse.py
@@ -1,7 +1,15 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+
+parser.add_argument(
+  "--start",
+  help="The starting line number of the ASCON bytes hex dump",
+  action="store"
+)
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
+  print(args)
   pass
