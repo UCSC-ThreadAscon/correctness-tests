fd8bdd95659351e7cd08d1bacc78c8e41bf7dbad
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-09-06 14:15:31 -0700

    Get the file from cmd line arguments.

--- parse.py
+++ parse.py
@@ -2,10 +2,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-helperText = "The starting line number of the ASCON bytes hex dump."
+helperText = "The file containing the OpenThread ASCON hex dump."
+parser.add_argument("--file", help=helperText, required=True)
+
+helperText = "The starting line number of the OpenThread ASCON hex dump."
 parser.add_argument("--start", help=helperText, required=True)
 
-helperText = "The ending line number of the ASCON bytes hex dump."
+helperText = "The ending line number of the OpenThread ASCON hex dump."
 parser.add_argument("--end", help=helperText, required=True)
 
 args = parser.parse_args()
